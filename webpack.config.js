68f5e73de69ce9a85a7bec2522d7e9ecd6ca445f
Carlos Usuga Martinez <klich84@gmail.com>
2021-11-15 16:28:18 -0500

    add dependencies files and css and sass settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
 	entry: './src/index.js',
@@ -24,6 +25,14 @@ module.exports = {
 				test: /\.html$/,
 				use: [{ loader: 'html-loader' }],
 			},
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
 		],
 	},
 	plugins: [
@@ -31,6 +40,9 @@ module.exports = {
 			template: './public/index.html',
 			filename: './index.html',
 		}),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
 	],
 	devServer: {
 		static: path.join(__dirname, 'dist'),
